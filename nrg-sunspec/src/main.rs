860814d738e66d26b41f375664f2a97eb773ddd2
Michael P. Jung <michael.jung@terreon.de>
2023-12-12 11:24:06 +0100

    Add config support to nrg-sunspec

--- nrg-sunspec/src/main.rs
+++ nrg-sunspec/src/main.rs
@@ -1,8 +1,7 @@
-use std::{error::Error, fs, net::SocketAddr, time::Duration};
+use std::{error::Error, fs, time::Duration};
 
-use clap::Parser;
 use nrg_mqtt::client::MqttClient;
-use rumqttc::{AsyncClient, MqttOptions, QoS};
+use rumqttc::QoS;
 use sunspec::tokio_modbus::{discover_models, read_model};
 use tokio::time::sleep;
 use tokio_modbus::{client::tcp::connect_slave, Slave};
@@ -12,12 +11,6 @@ use tracing_subscriber::FmtSubscriber;
 use crate::config::Config;
 
 pub mod config;
-
-#[derive(Parser)]
-struct Args {
-    addr: SocketAddr,
-    device_id: u8,
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -31,8 +24,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let data = String::from_utf8(data).expect("Config file contains non-utf8 characters");
     let cfg: Config = toml::from_str(&data).expect("Error in config file");
 
-    let args = Args::parse();
-    let mut ctx = connect_slave(args.addr, Slave(args.device_id)).await?;
+    let mut ctx = connect_slave(cfg.modbus.addr, Slave(cfg.modbus.slave)).await?;
 
     let models = discover_models(&mut ctx).await?.models;
     let m1 = read_model(&mut ctx, &models.m1).await?;
@@ -47,18 +39,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     loop {
         let m103 = read_model(&mut ctx, &models.m103).await?;
-        println!("{:#?}", m103);
         let w = m103.w as f32 * 10f32.powf(m103.w_sf.into());
         let wh = m103.wh as f32 * 10f32.powf(m103.wh_sf.into());
-
-        /*
-        let m701 = read_model(&mut ctx, &models.m704).await?;
-        println!("{:#?}", m701);
-        */
-        /*
-        let w = m701.w.unwrap_or(0) as f32 * 10f32.powf(m701.w_sf.unwrap_or(0).into());
-        let wh = m701.tot_wh_inj.unwrap_or(0) as f32 * 10f32.powf(m701.tot_wh_sf.unwrap_or(0).into());
-        */
 
         println!("{:12.3} kWh {:9.3} kW", wh / 1000.0, w / 1000.0,);
 
